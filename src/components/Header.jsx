f5badbe2d0f2f095893c7aeb6b6119b431f5a138
Lucas Eifler <lucass.eifler@gmail.comm>
2024-03-27 12:28:38 +0100

    adding tailwind in correct way

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ const Header = () => {
         <header className="bg-neutral-800 p-10">
             <div className="text-center">
                 <h3 className="text-xl text-neutral-500 py-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
-                <p></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, nihil?</p>
             </div>
         </header>
     )
