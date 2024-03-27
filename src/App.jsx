66a2401ea7a91dcd70978f20ceb8376d890515c9
Lucas Eifler <lucass.eifler@gmail.comm>
2024-03-27 11:51:36 +0100

    adding components folder and files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './output.css'
+import Header from "./components/Header";
+import DataSection from "./components/DataSection";
+import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="flex-col h-screen justify-between  bg-neutral-900">
-      <div className='bg-black'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  function App() {
+    return (
+        <div className="flex-col h-screen justify-between  bg-neutral-900">
+        <Header />
+        <DataSection />
       </div>
-      <h1 className='text-lime-600'>Vite + React</h1>
-      <div className="flex justify-center">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-  </div>
-  )
-}
-
-export default App
+    );
+  }
+  
+  export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header className="bg-neutral-800 p-10">
+            <div className="text-center">
+                <h3 className="text-xl text-neutral-500 py-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+                <p></p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
